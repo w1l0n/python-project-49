2e6e60f135e7c0cc2f8e343261ac72ae75dd0f64
Evgenii Nikonorov <wildon@mail.ru>
2024-06-23 13:56:05 +0400

    universal mechanics for games

---
+++ brain_games/game_mechanics.py
@@ -0,0 +1,23 @@
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    return name
+
+
+def ask_question(question):
+    print(f'Question: {question}')
+    return prompt.string(f'Your answer: ')
+
+
+def play_round(question, correct_answer):
+    user_answer = ask_question(question)
+    if user_answer == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'")
+        return False
