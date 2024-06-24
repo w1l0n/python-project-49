744303ec755c27ca3caa5bd9343c8c13c8678053
Evgenii Nikonorov <wildon@mail.ru>
2024-06-24 19:19:05 +0400

    game_mechanics.py

--- brain_games/games/game_mechanics.py
+++ brain_games/games/game_mechanics.py
@@ -19,5 +19,6 @@ def play_round(question, correct_answer):
         print('Correct!')
         return True
     else:
-        print(f'"{user_answer}" is wrong answer ;(. Correct answer was "{correct_answer}"')
+        print(f'"{user_answer}" is wrong answer ;(. '
+              f'Correct answer was "{correct_answer}"')
         return False
